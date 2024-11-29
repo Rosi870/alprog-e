16ea230a6e28d36674ee1cf117334a19bea1f68d
Rosi870 <anasudrajat27@gmail.com>
2024-11-29 09:51:37 +0800

    Add files via upload

---
+++ tugas8.py
@@ -0,0 +1,32 @@
+# ARITMATIKA
+
+x = 24
+y = 57
+
+# penjumlahan
+hasil = x + y
+print("nilai dari x + y =", hasil)
+
+# pengurangan
+hasil = x - y
+print("nilai dari x - y =", hasil)
+
+# pembagian
+hasil = x / y
+print("nilai dari x / y =", hasil)
+
+# perkalian
+hasil = x * y
+print("nilai dari x * y =", hasil)
+
+# eksponen (perpangkatan)
+hasil = x ** y
+print("hasil dari x ** y =", hasil)
+
+# modulus (sisa bagi)
+hasil = x % y
+print("hasil dari x % y =", hasil)
+
+# floor division
+hasil = x // y
+print("hasil dari x //  y =", hasil)
